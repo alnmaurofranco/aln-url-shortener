9c1ef3bde5b55a71553e8cb76a7c853ad4e01c44
alnmaurofranco <jefeson23@live.com>
2021-08-06 18:03:45 -0300

    🚀 deploy

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333/api',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3333/api'
+      : 'https://api-alnurlshortener.herokuapp.com/api',
 })
 
 export { api }
